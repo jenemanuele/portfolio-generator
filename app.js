e179e91584e14611897f049e03e2b448f4329b87
Jennifer Emanuele <jemanuele@gmail.com>
2021-12-28 11:16:22 -0500

    fixed question progression issue

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generatePage = require('./src/page-template');
 
 
 const promptUser = () => {
@@ -7,23 +8,42 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                    return false;
+                }                
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub Username?'            
-        },
-        {
-            type: 'input',
-            name:'github',
-            message: 'Enter your Github Username'
-        },
+            message: 'Enter your GitHub Username? (Required)',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your Github username!');
+                    return false;
+                }                
+            }            
+        }, 
+            {
+                type: 'confirm',
+                name: 'confirmAbout',
+                message: 'Would you like to enter some information about yourself for an "About" section?',
+                default: true
+            },    
         {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself:'
-        }
+            message: 'Provide some information about yourself:',
+            when: ({ confirmAbout }) => {            
+            }
+        }    
     ]);
 };
 
@@ -43,12 +63,28 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else{
+                    console.log('You need to enter a project name!');
+                    return false;
+                }               
+            }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description!');
+                    return false;
+                }                
+            }
         },
         {
             type: 'checkbox',
@@ -59,7 +95,15 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: githubLink => {
+                if (githubLink) {
+                    return true;
+                } else {
+                    console.log('Please enter a Github link!');
+                    return false;
+                }                
+            }
         },
         {
             type: 'confirm',
@@ -74,28 +118,20 @@ const promptProject = portfolioData => {
             default: false
         }
     ])
-    .then(projectData => {
-        portfolioData.projects.push(projectData);
-        if (projectData.confirmAddProject) {
-            return promptProject(portfolioData);
-        } else {
-            return portfolioData;
-        }
-    });
+    // .then(projectData => {
+    //     portfolioData.projects.push(projectData);
+    //     if (projectData.confirmAddProject) {
+    //         return promptProject(portfolioData);
+    //     } else {
+    //         return portfolioData;
+    //     }
+    // });
 };
 
 promptUser()
-    .then(promptProject)
+   .then(promptProject)
     .then(portfolioData => {
         console.log(portfolioData);
-    });
-
-
-
-
-//const fs = require('fs');
-// const generatePage = require('./src/page-template.js'); 
-
 // const pageHTML = generatePage(name, github);
 
 // fs.writeFile('index.html', pageHTML, err => {
@@ -103,3 +139,4 @@ promptUser()
 
 //     console.log('Portfolio complete!  Check out index.html to see the output!');
 // });
+});
