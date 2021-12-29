c05e2949a49fad17afcc6db2e0455c056ef968ca
Jennifer Emanuele <jemanuele@gmail.com>
2021-12-29 11:42:01 -0500

    error issue

--- app.js
+++ app.js
@@ -116,24 +116,33 @@ const promptProject = portfolioData => {
         }
     ])
     // .then(projectData => {
-    //     portfolioData.projects.push(projectData);
+    //      portfolioData.projects.push(projectData);
     //     if (projectData.confirmAddProject) {
-    //         return promptProject(portfolioData);
+    //          return promptProject(portfolioData);
     //     } else {
     //         return portfolioData;
-    //     }
-    //     });
-    // };
+    //    }
+    //    });
+     };
 
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
     const pageHTML = generatePage(portfolioData);
 
-        fs.writeFile('./index.html', pageHTML, err => {
-                if (err) throw new Error(err);
-
-        console.log('Page created! Check out index.html in this directory to see it!');
+        fs.writeFile('./dist/index.html', pageHTML, err => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log('Page created! Check out index.html in this directory to see it!');
+            fs.copyFile('./src/styole.css', './dis/style.css', err => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                console.log('Style sheet copied successfully!');
+            });
         });
     });
 
